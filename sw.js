ed8b5668aacdeaafeeea73bea193d649d68d46c8
Temesgen Yirdaw <tames4ever@gmail.com>
2026-02-26 17:50:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
 self.addEventListener("install", e=>{
   e.waitUntil(
     caches.open("anbesa").then(cache=>{
-      return cache.addAll(["./","index.html","app.js","manifest.json","logo.png","bingo_cards.json"]);
+      return cache.addAll([
+        "./",
+        "index.html",
+        "app.js",
+        "manifest.json",
+        "logo.png",
+        "bingo_cards.json"
+      ]);
     })
   );
 });
 
 self.addEventListener("fetch", e=>{
   e.respondWith(
-    caches.match(e.request).then(res=> res || fetch(e.request))
+    caches.match(e.request).then(res=>res || fetch(e.request))
   );
 });
